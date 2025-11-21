0ee5b70b2f4c0dc6d7be758b8b4400172a21bf10
MiguelStael <miguel.stael@gmail.com>
2025-11-21 00:24:21 -0300

    Tela de Login quase completa

--- src/pages/Login/style.ts
+++ src/pages/Login/style.ts
@@ -1,14 +1,57 @@
 import { StyleSheet } from 'react-native';
 
-export const styles = StyleSheet.create({
+export default StyleSheet.create({
     container: {
         flex: 1,
-        backgroundColor: '#fff',
-        alignItems: 'center',
         justifyContent: 'center',
+        alignItems: 'center',
+        paddingHorizontal: 20,
     },
+
+    box: {
+        width: '90%',
+        backgroundColor: '#fff',
+        borderRadius: 20,
+        padding: 25,
+        alignItems: 'center',
+        elevation: 5,
+    },
+
     title: {
-        fontSize: 20,
+        fontSize: 28,
         fontWeight: 'bold',
-    }
+        marginBottom: 20,
+        color: '#000',
+    },
+
+    input: {
+        width: '100%',
+        height: 45,
+        backgroundColor: '#b3dcee',
+        borderRadius: 20,
+        paddingHorizontal: 15,
+        marginBottom: 15,
+    },
+
+    button: {
+        width: '100%',
+        height: 45,
+        backgroundColor: '#feb680',
+        borderRadius: 20,
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginTop: 10,
+    },
+
+    buttonText: {
+        fontSize: 16,
+        fontWeight: 'bold',
+        color: '#000',
+    },
+
+    link: {
+        marginTop: 15,
+        color: '#0044aa',
+        textDecorationLine: 'underline',
+    },
 });
--- src/routers/index.tsx
+++ src/routers/index.tsx
@@ -1,9 +1,9 @@
-import { NavigationContainer } from "@react-navigation/native";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-import Home from '../pages/Home';
 import Login from '../pages/Login';
-import Cadastro from '../pages/Cadastro'; 
+import Home from '../pages/Home';
+import Cadastro from '../pages/Cadastro';
 import Perfil from '../pages/Perfil';
 import Favoritos from '../pages/Favoritos';
 import Admin from '../pages/Admin';
@@ -11,12 +11,12 @@ import Cursos from '../pages/Cursos';
 
 const Stack = createNativeStackNavigator();
 
-export const Routers = () => {
+export function Routers() {
   return (
     <NavigationContainer>
-      <Stack.Navigator screenOptions={{ headerShown: false }}>
-        <Stack.Screen name="StackHome" component={Home} />
+      <Stack.Navigator initialRouteName="StackLogin" screenOptions={{ headerShown: false }}>
         <Stack.Screen name="StackLogin" component={Login} />
+        <Stack.Screen name="StackHome" component={Home} />
         <Stack.Screen name="StackCadastro" component={Cadastro} />
         <Stack.Screen name="StackPerfil" component={Perfil} />
         <Stack.Screen name="StackFavoritos" component={Favoritos} />
@@ -25,4 +25,4 @@ export const Routers = () => {
       </Stack.Navigator>
     </NavigationContainer>
   );
-};
+}
