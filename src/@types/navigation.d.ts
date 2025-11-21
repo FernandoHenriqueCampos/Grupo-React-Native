32041c58ad93266798b269bbc50b227caab33a25
MiguelStael <miguel.stael@gmail.com>
2025-11-21 19:53:31 -0300

    substituindo navigation.d.ts por types.ts/ adicionando imagem background de cadastro

--- src/@types/navigation.d.ts
+++
@@ -1,9 +0,0 @@
-export declare global {
-  namespace ReactNavigation {
-    interface RootParamList {
-      StackLoading: undefined;
-      StackHome: undefined;
-      StackLogin: undefined;
-    }
-  }
-}
