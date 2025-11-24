2a9c088fda4cdfe50b7971793b3eb9263a2a770c
FERNANDO H  M CAMPOS <braaboozx@gmail.com>
2025-11-24 18:51:31 -0300

    Correção style page cursos, agora no style.ts

--- src/pages/Cursos/index.tsx
+++ src/pages/Cursos/index.tsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { View, Text, StyleSheet, FlatList, ActivityIndicator, SafeAreaView } from 'react-native';
 import CardCursos from '../../components/CardCursos';
 import { fetchMSLearnCourses, Course } from '../../services/courseService';
+import { styles } from './style';
 
 const PageCursos: React.FC = () => {
     const [courses, setCourses] = useState<Course[]>([]);
@@ -44,40 +45,6 @@ const PageCursos: React.FC = () => {
     );
 };
 
-// ➡️ Estilos do Componente
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#f0f4f7',
-    },
-    header: {
-        fontSize: 24,
-        fontWeight: 'bold',
-        textAlign: 'center',
-        marginTop: 10,
-        marginBottom: 5,
-        color: '#333',
-    },
-    listContent: {
-        paddingBottom: 20,
-    },
-    loadingContainer: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: '#f0f4f7',
-    },
-    loadingText: {
-        marginTop: 10,
-        fontSize: 16,
-        color: '#0078d4',
-    },
-    errorText: {
-        fontSize: 16,
-        color: '#666',
-        textAlign: 'center',
-        padding: 20
-    }
-});
+
 
 export default PageCursos;
