ff3bb3d85462e302cffaa87de124f24b267de766
jessesiqueira-dev <jsiqueira397vendas@gmail.com>
2025-11-25 14:21:27 -0300

    ajustes na url da api

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,8 +1,8 @@
 import axios from "axios";
 
-export const BASE_URL_PETS = process.env.EXPO_PUBLIC_API_BASE_URL_SHOP;
+export const BASE_URL_PETS = process.env.EXPO_PUBLIC_API_URL;
 export const BASE_URL_USUARIOS = process.env.EXPO_PUBLIC_API_URL_USUARIOS;
-export const USE_MOCK = process.env.EXPO_PUBLIC_USE_MOCK_DATA === "true";
+export const USE_MOCK = process.env.EXPO_PUBLIC_USE_MOCK_DATA === "false";
 
 export const apiPets = axios.create({
   baseURL: BASE_URL_PETS,
